dfc980d629209180dd101af3c617a0bd4eb93fcb
GuillaumeDelferiere <guigui.delferiere@gmail.com>
2024-11-29 10:16:02 +0100

    update des tables

--- mvp_enquete/ui/structure_table.py
+++ mvp_enquete/ui/structure_table.py
@@ -12,7 +12,8 @@ CREATE TABLE IF NOT EXISTS affaire (
     type_crime TEXT NOT NULL,
     lieu TEXT NOT NULL,
     etat TEXT NOT NULL,
-    date_ouverture DATE NOT NULL
+    date_ouverture DATE NOT NULL,
+    enqueteur_assigne TEXT
 )
 ''')
 
@@ -20,7 +21,16 @@ CREATE TABLE IF NOT EXISTS affaire (
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS preuve (
     id_preuve INTEGER PRIMARY KEY AUTOINCREMENT,
-    nom_preuve TEXT NOT NULL
+    nom_preuve TEXT NOT NULL, 
+    type_preuve TEXT NOT NULL,
+    description TEXT, 
+    lien TEXT, 
+    lieu_collecte TEXT, 
+    date_heure_decouverte DATE NOT NULL,
+    resultat_analyse TEXT, 
+    etat_actuel TEXT,
+    scientifique_en_charge TEXT, 
+    FOREIGN KEY (enquete_id) REFERENCES enquete(id)
 )
 ''')
 
@@ -29,6 +39,43 @@ cursor.execute('''
 CREATE TABLE IF NOT EXISTS rapport (
     id_rapport INTEGER PRIMARY KEY AUTOINCREMENT,
     contenu TEXT NOT NULL
+)
+''')
+
+# Création de la table Suspects
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS suspects (
+    id_suspects INTEGER PRIMARY KEY AUTOINCREMENT,
+    enquete_id INTEGER NOT NULL,
+    nom_suspects TEXT, 
+    prenom_suspects TEXT,
+    surnom TEXT,
+    age INTEGER,
+    date_naissance DATE,
+    lieu_naissance TEXT,
+    adresse TEXT,
+    taille INTEGER,
+    poids INTEGER,
+    signes_particuliers TEXT,
+    role TEXT,
+    alibi_declare TEXT,
+    FOREIGN KEY (enquete_id) REFERENCES enquete(id)
+)
+''')
+
+# Création de la table Témoin
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS temoin (
+    id_temoin INTEGER PRIMARY KEY AUTOINCREMENT,
+    enquete_id INTEGER NOT NULL,
+    nom_temoin TEXT,
+    prenom_temoin TEXT,
+    age INTEGER, 
+    date_temoignage DATE,
+    temoignage TEXT,
+    adresse TEXT,
+    numero_telephone TEXT,
+
 )
 ''')
 
